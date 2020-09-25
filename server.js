87816732d162a570a9a1cae23ecbaf65e959305e
maalleekk <48369747+maalleekk@users.noreply.github.com>
2020-09-25 13:10:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.route('/api')
     console.log('GET request detected');
   })
   .post((req, res) => {
-    res.send('hello world);
+    res.send('hello world');
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
   });
