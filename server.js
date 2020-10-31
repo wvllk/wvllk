6159f5513b22cfe9073a61381e2a2f3612f050ba
maalleekk <48369747+maalleekk@users.noreply.github.com>
2020-10-31 14:52:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+
+Lealena Befikadu
+2:51 PM (0 minutes ago)
+
+to me
+
 // These are our required libraries to make the server work.
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
+import fetch from 'node-fetch';
 import express from 'express';
 import dotenv from 'dotenv';
-import fetch from 'node-fetch';
+import countries from './public/lab_6/countries.js';
 
 dotenv.config();
 
@@ -14,20 +21,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.route('/api')
-  .get((req, res) => {
-    console.log('GET request detected');
-    res.send(`Lab 5 for ${process.env.NAME}`);
-  })
-  .post((req, res) => {
-    console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-  });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
+// app.route('/api')
+//   .get((req, res) => {
+//     console.log('GET request detected');
+//     res.send(`Lab 5 for ${process.env.NAME}`);
+//   })
 app.route('/api')
   .get(async(req, res) => {
     console.log('GET request detected');
@@ -40,3 +44,13 @@ app.route('/api')
     const json = await data.json();
     res.json(json);
   });
+
+  // .post((req, res) => {
+  //   res.json(countries);
+  //   console.log('POST request detected');
+  //   console.log('Form data in res.body', req.body);
+  // });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
