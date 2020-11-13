7a9be45dc7fc3118f33f0bed9e541e487a0f87e7
maalleekk <48369747+maalleekk@users.noreply.github.com>
2020-11-13 10:21:01 -0500

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,7 +1,7 @@
 // You may wish to find an effective randomizer function on MDN.
 
-//const { reverse } = require("cypress/types/lodash");
-//const { server } = require("cypress/types/sinon");
+const { reverse } = require("cypress/types/lodash");
+const { server } = require("cypress/types/sinon");
 
 function range(int) {
   const arr = [];
@@ -14,36 +14,34 @@ function range(int) {
 function sortFunction(a, b, key) {
   if (a[key] < b[key]) {
     return -1;
-  } if (a[key] > b[key]) {
+  }
+  if (a[key] > b[key]) {
     return 1;
   }
   return 0;
 }
 
-document.body.addEventListener('submit', async (e) => {
+document.body.addEventListener("submit", async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
-  fetch('/api', {
-    method: 'POST',
+  fetch("/api", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(form)
+    body: JSON.stringify(form),
   })
-
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      if (document.querySelector('.flex-inner')) {
-        document.querySelector('.flex-inner').remove();
-      }
-
-      const arr1 = range(10);
+      if (document.querySelector('.flex-inner'.remove
+  
+      const arr1 = range=(10);
       const arr2 = arr1.map(() => {
         const number = getRandomIntInclusive(0, 243);
         return fromServer[number];
       });
       const revlist = arr2.sort((a, b) => sortFunction(b, a, 'name'));
-      const olist = document.createElement('ol');
+      const olist = document.createElement('ol')
       olist.className = 'flex-inner';
       $('form').append(olist);
 
@@ -55,4 +53,4 @@ document.body.addEventListener('submit', async (e) => {
       });
     })
     .catch((err) => console.log(err));
-});
+})
